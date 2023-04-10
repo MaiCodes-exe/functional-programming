84a901457a4d445a65273a70b21328fe87cc6444
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-10 14:43:43 +0000

    5th done

--- exercises/5.js
+++ exercises/5.js
@@ -1,1 +1,10 @@
 // Pass Arguments to Avoid External Dependence in a Function
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(b) {
+let a = 1;
+return fixedValue + a
+  // Only change code above this line
+}
