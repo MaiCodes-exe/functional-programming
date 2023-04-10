47ba7ff186a93c7004009c87e0d5b867c582d965
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-10 14:36:56 +0000

    4th done

--- exercises/4.js
+++ exercises/4.js
@@ -1,1 +1,12 @@
 // Avoid Mutations and Side Effects Using Functional Programming
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+
+  for (let i = 0; i < fixedValue; i++){
+    return fixedValue + 1;
+  }
+  // Only change code above this line
+}
