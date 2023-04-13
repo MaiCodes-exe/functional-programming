ecaf22bd0f9a4b7ffe0f4420787f96fd5ab178b0
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-13 21:07:18 +0000

    10th done, need reviewing

--- exercises/10.js
+++ exercises/10.js
@@ -1,4 +1,20 @@
 // Implement the filter Method on a Prototype
 
 
-// need to ask about this one
+Array.prototype.myFilter = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+  for(let i = 0 ; i < this.length; i++){
+    if (callback(this[i], i, this)){
+      newArray.push(this[i])
+    }
+    
+  }
+  // Only change code above this line
+  return newArray;
+};
+
+
+// var myCallback = (element, index, array) => {
+
+// }
