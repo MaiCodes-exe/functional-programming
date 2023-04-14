4875661e9f3d640053de9ec25ccb6b855a85c222
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-14 17:38:26 +0000

    12th done

--- exercises/12.js
+++ exercises/12.js
@@ -1,1 +1,11 @@
 // Remove Elements from an Array Using slice Instead of splice
+function nonMutatingSplice(cities) {
+  // Only change code below this line
+  // console.log(cities.splice(0,3))
+  return cities.slice(0,3);
+
+  // Only change code above this line
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
