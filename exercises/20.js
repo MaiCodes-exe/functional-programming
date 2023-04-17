cfaa15dd9571266a997260a1f0887d11ea700564
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-17 20:44:11 +0000

    20th done

--- exercises/20.js
+++ exercises/20.js
@@ -1,1 +1,8 @@
 // Combine an Array into a String Using the join Method
+
+function sentensify(str) {
+  // Add your code below this line
+  return str.split(/\W/).join(" ");
+  // Add your code above this line
+}
+sentensify("May-the-force-be-with-you");
