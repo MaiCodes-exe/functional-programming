5618a085efc2f77af9849f714baa723df909f02d
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-17 20:36:27 +0000

    19th done

--- exercises/19.js
+++ exercises/19.js
@@ -1,1 +1,10 @@
 // Split a String into an Array Using the split Method
+function splitify(str) {
+  // Only change code below this line
+  // console.log(str.split(/\W/))
+return str.split(/\W/)
+
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
