1a0bd2ba54d8894c4801b56eb2bc872f51cc9fea
MaiCodes-exe <maise.b.costa@gmail.com>
2023-04-17 20:33:09 +0000

    18th done sorta

--- exercises/18.js
+++ exercises/18.js
@@ -1,1 +1,10 @@
 // Return a Sorted Array Without Changing the Original Array
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Add your code below this line
+  return [].concat(arr).sort(function(a, b) {
+    return a - b;
+  });
+  // Add your code above this line
+}
+nonMutatingSort(globalArray);
